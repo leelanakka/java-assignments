074ded594d7f4c25353f74628cf72cdc99fbb984
leelanakka <44019459+leelanakka@users.noreply.github.com>
2019-03-05 20:46:55 +0530

    added area calculator calss

---
+++ src/com/step/assignments/AreaCalculator.java
@@ -0,0 +1,21 @@
+package com.step.assignments;
+
+public class AreaCalculator {
+    public static void main(String[] args) {
+
+    }
+
+    public static double area(double radius){
+        if(radius < 0){
+            return -1.0;
+        }
+        return (double) radius*radius*Math.PI;
+    }
+
+    public static double area(double x,double y){
+        if(x < 0 || y<0){
+            return -1.0;
+        }
+        return (double) x*y;
+    }
+}
