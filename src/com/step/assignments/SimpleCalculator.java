9000b9ee646c03cf6989d502883d9cb40f9bf725
leelanakka <44019459+leelanakka@users.noreply.github.com>
2019-03-10 00:22:55 +0530

    added simle calculator file

---
+++ src/com/step/assignments/SimpleCalculator.java
@@ -0,0 +1,57 @@
+package com.step.assignments;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+    }
+    private double firstNumber;
+    private double secondNumber;
+
+    public SimpleCalculator() {
+    }
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult(){
+        return this.firstNumber+this.secondNumber;
+    }
+
+    public double getSubtractionResult(){
+        return this.firstNumber-this.secondNumber;
+    }
+
+    public double getMultiplicationResult(){
+        return this.firstNumber*this.secondNumber;
+    }
+
+    public double getDivisionResult(){
+        if(this.secondNumber ==0){
+            return 0;
+        }
+        return this.firstNumber/this.secondNumber;
+    }
+
+}
