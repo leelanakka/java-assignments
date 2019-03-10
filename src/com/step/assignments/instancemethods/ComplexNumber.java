95d5849f9ed633253f65deef9b909ab202f4ff87
leelanakka <44019459+leelanakka@users.noreply.github.com>
2019-03-10 15:00:10 +0530

    added complex number problem
    it will add and substract the complex numbers

---
+++ src/com/step/assignments/instancemethods/ComplexNumber.java
@@ -0,0 +1,55 @@
+package com.step.assignments.instancemethods;
+
+public class ComplexNumber {
+    public static void main(String[] args) {
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
+
+
+    }
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real,double imaginary){
+        this.real+=real;
+        this.imaginary+=imaginary;
+    }
+
+    public void add(ComplexNumber other){
+        this.real += other.real;
+        this.imaginary += other.imaginary;
+    }
+
+    public void subtract(double real,double imaginary){
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber other){
+        this.real -= other.real;
+        this.imaginary -= other.imaginary;
+    }
+}
